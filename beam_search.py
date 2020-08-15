d0e82b3fcbfe5c7f8e878afc0023d208d80cabb5
chaoji <chocobo1985@gmail.com>
2020-08-15 08:25:01 -0400

    fixed import statement

--- beam_search.py
+++ beam_search.py
@@ -2,8 +2,8 @@
 import tensorflow as tf
 import numpy as np
 
-from data.tokenization import SOS_ID
-from data.tokenization import EOS_ID
+from tokenization import SOS_ID
+from tokenization import EOS_ID
 
 
 # large negative value considered numerically as -inf
