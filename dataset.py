22c60af210c53f21c194c882b510558bdba7f36f
chaoji <chocobo1985@gmail.com>
2020-09-09 12:53:49 -0400

    added dataset builders for pretraining and finetuning sequence classifier on IMDB dataset

--- dataset.py
+++ dataset.py
@@ -15,6 +15,120 @@ _MIN_BOUNDARY = 8
 _BOUNDARY_SCALE = 1.1
 
 
+def create_and_preprocess(filenames, 
+                          parse_fn,
+                          shuffle, 
+                          num_parallel_calls,
+                          filter_fn=None, 
+                          buffer_size_per_file=None, 
+                          random_seed=None):
+  """Initialize tf.data.Dataset instance and apply parsing (deserialization), 
+  shuffling and filtering preprocessing transforms.
+
+  Args:
+    filenames: a list of strings, names of TFRecord files.
+    parse_fn: callable, function that deserializes protobuf message.
+    shuffle: bool scalar, if False, the training examples will be generated
+      deterministically. 
+    num_parallel_calls: int scalar, num of TFRecord files to be processed
+      concurrently.
+    filter_fn: callable, function that applies filtering.
+    buffer_size_per_file: int scalar, buffer size used to shuffle records in a 
+      single *.tfrecord file.
+    random_seed: int scalar, random seed. 
+
+  Returns:
+    dataset: a tf.data.Dataset instance. 
+  """
+  # shape: ()
+  dataset = tf.data.Dataset.from_tensor_slices(filenames).shuffle(
+      len(filenames), seed=random_seed)
+
+  # set `options.experimental_deterministic` to False to shuffle the dataset 
+  # shape: ()
+  options = tf.data.Options()
+  options.experimental_deterministic = False if shuffle else True
+
+  if buffer_size_per_file is not None:
+    map_fn = lambda filename: tf.data.TFRecordDataset(
+        filename, buffer_size=_READ_RECORD_BUFFER).shuffle(
+        buffer_size_per_file)
+  else:
+    map_fn = lambda filename: tf.data.TFRecordDataset(
+        filename, buffer_size=_READ_RECORD_BUFFER)
+
+  dataset = dataset.interleave(
+      map_fn, cycle_length=num_parallel_calls,
+      num_parallel_calls=tf.data.experimental.AUTOTUNE).with_options(options)
+  dataset = dataset.map(parse_fn, num_parallel_calls=num_parallel_calls)
+  # filter out long sequences
+  if filter_fn is not None:
+    dataset = dataset.filter(filter_fn)
+
+  return dataset
+
+
+def parse_fn_sequence_pair(serialized_example):
+  """Deserializes a protobuf message into a source and target (batched) sequence
+  of token IDs for sequence transduction.
+
+  Args:
+    serialized_example: string scalar tensor, serialized example of 
+      a source-target pair.
+
+  Returns:
+    src_tokens_ids: int tensor of shape [src_seq_len], token ids of source
+      sequence.
+    tgt_tokens_ids: int tensor of shape [tgt_seq_len], token ids of target
+      sequence.
+  """
+  parse_dict = {'source': tf.io.VarLenFeature(tf.int64),
+                'target': tf.io.VarLenFeature(tf.int64)}
+
+  parsed = tf.io.parse_single_example(serialized_example, parse_dict)
+  src_token_ids = tf.sparse.to_dense(parsed['source'])
+  tgt_token_ids = tf.sparse.to_dense(parsed['target'])
+  return src_token_ids, tgt_token_ids
+
+
+def parse_fn_single_sequence(serialized_example):
+  """Deserializes a protobuf message into a single (batched) sequence of token 
+  IDs.
+
+  Args:
+    serialized_example: string scalar tensor, serialized example of 
+      a token IDs.
+
+  Returns:
+    token_ids: int tensor of shape [seq_len], token ids.
+  """
+  parse_dict = {'token_ids': tf.io.VarLenFeature(tf.int64)}
+  parsed = tf.io.parse_single_example(serialized_example, parse_dict)
+  token_ids = tf.sparse.to_dense(parsed['token_ids'])
+  return token_ids
+
+
+def parse_fn_sequence_classification(serialized_example):
+  """Deserializes a protobuf message into a single (batched) sequence of token
+
+  Args:
+    serialized_example: string scalar tensor, serialized example of 
+      a token IDs and sequence label.
+  
+  Returns:
+    token_ids: int tensor of shape [seq_len], token_ids.
+    label: int scalar tensor, sequence class label.
+  """
+  parse_dict = {'token_ids': tf.io.VarLenFeature(tf.int64),
+                'label': tf.io.VarLenFeature(tf.int64)}
+
+  parsed = tf.io.parse_single_example(serialized_example, parse_dict)
+  token_ids = tf.sparse.to_dense(parsed['token_ids'])
+  label = tf.sparse.to_dense(parsed['label'])
+
+  return token_ids, label
+
+
 class BaseSequenceTransductionDatasetBuilder(object):
   """Abstract base class for building sequence transduction dataset.
 
@@ -38,28 +152,18 @@ class BaseSequenceTransductionDatasetBuilder(object):
         zero-padded to the length of the longest sequence in each batch, so
         the last column contains at least one non-zero (padded) token.
     """
-    # shape: ()
-    dataset = tf.data.Dataset.from_tensor_slices(filenames).shuffle(
-        len(filenames), seed=self._random_seed)
 
-    # set `options.experimental_deterministic` to False to shuffle the dataset 
-    # shape: ()
-    options = tf.data.Options()
-    options.experimental_deterministic = False if self._shuffle else True
-    dataset = dataset.interleave(
-        lambda filename: tf.data.TFRecordDataset(
-            filename, buffer_size=_READ_RECORD_BUFFER),
-        cycle_length=self._num_parallel_calls,
-        num_parallel_calls=tf.data.experimental.AUTOTUNE).with_options(options)
+    filter_fn = lambda x, y: tf.logical_and(
+        tf.size(x) <= self._max_length, tf.size(y) <= self._max_length)
 
-    # shape: ((None,), (None,))
-    dataset = dataset.map(
-        _parse_example, num_parallel_calls=self._num_parallel_calls)
-
-    # filter out long sequences
-    dataset = dataset.filter(lambda x, y: tf.logical_and(
-        tf.size(x) <= self._max_length,
-        tf.size(y) <= self._max_length))
+    dataset = create_and_preprocess(
+        filenames,
+        parse_fn=parse_fn_sequence_pair,
+        shuffle=self._shuffle,
+        num_parallel_calls=self._num_parallel_calls,
+        filter_fn=filter_fn,
+        buffer_size_per_file=None,
+        random_seed=self._random_seed)
 
     dataset = self._batch_examples(dataset)
     dataset = dataset.repeat(-1)
@@ -259,24 +363,115 @@ class StaticBatchDatasetBuilder(BaseSequenceTransductionDatasetBuilder):
         window_size=self._batch_size))
 
 
-def _parse_example(serialized_example):
-  """Parses a protobuf-serialized example of a pair of source and target
-  sequence.
-
-  Args:
-    serialized_example: string scalar tensor, serialized example of 
-      a source-target pair.
-
-  Returns:
-    src_tokens_ids: int tensor of shape [src_seq_len], token ids of source
-      sequence.
-    tgt_tokens_ids: int tensor of shape [tgt_seq_len], token ids of target
-      sequence.
+class SequenceClassifierDatasetBuilder(object):
+  """Builds a tf.data.Dataset instance that generates batched sequences to token
+  IDs (without class label) for pretraining or (with class label) for 
+  finetuning the sequence classifier for IMDB movie review dataset.
   """
-  parse_dict = {'source': tf.io.VarLenFeature(tf.int64), 
-                'target': tf.io.VarLenFeature(tf.int64)}
+  def __init__(self,
+               batch_size,
+               shuffle,
+               max_length,
+               num_parallel_calls,
+               num_buckets=10,
+               bucket_width=100,
+               random_seed=None):
+    """Constructor.
 
-  parsed = tf.io.parse_single_example(serialized_example, parse_dict)
-  src_token_ids = tf.sparse.to_dense(parsed['source'])
-  tgt_token_ids = tf.sparse.to_dense(parsed['target'])
-  return src_token_ids, tgt_token_ids
+    Args:
+      batch_size: int scalar, batch size.
+      shuffle: bool scalar, if False, the training examples will be generated
+        deterministically.
+      max_length: int scalar, sequences longer than this will be filtered out. 
+      num_parallel_calls: int scalar, num of TFRecord files to be processed
+        concurrently.
+      num_buckets: int scalar, num of sequence length buckets. 
+      bucket_width: int scalar, size of each sequence length bucket.
+      random_seed: int scalar, random seed.
+    """
+    self._batch_size = batch_size
+    self._shuffle = shuffle
+    self._max_length = max_length
+    self._num_parallel_calls = num_parallel_calls
+    self._num_buckets = num_buckets
+    self._bucket_width = bucket_width
+    self._random_seed = random_seed
+
+  def build_pretrain_dataset(self, filenames):
+    """Builds dataset for pretraining.
+
+    Args:
+      filenames: a list of strings, names of TFRecord files.
+  
+    Returns:
+      dataset: a tf.data.Dataset instance, each item is an int tensors
+        of shape [batch_size, seq_len] holding the token ids. 
+    """
+    filter_fn = (None if self._max_length is None
+        else lambda x: tf.size(x) <= self._max_length)
+
+    dataset = create_and_preprocess(
+        filenames,
+        parse_fn=parse_fn_single_sequence,
+        shuffle=self._shuffle,
+        num_parallel_calls=self._num_parallel_calls,
+        filter_fn=filter_fn,
+        buffer_size_per_file=750,
+        random_seed=self._random_seed)
+
+    def example_to_bucket_id(token_ids):
+      seq_len = tf.size(token_ids)
+      bucket_id = seq_len // self._bucket_width
+      return tf.cast(tf.minimum(self._num_buckets - 1, bucket_id), 'int64')
+
+    def batching_fn(bucket_id, grouped_dataset):
+      return grouped_dataset.padded_batch(
+          self._batch_size, (None,), drop_remainder=True)
+
+    dataset = dataset.apply(tf.data.experimental.group_by_window(
+        key_func=example_to_bucket_id,
+        reduce_func=batching_fn,
+        window_size=self._batch_size))
+    dataset = dataset.repeat(-1)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    return dataset
+
+  def build_finetune_dataset(self, filenames):
+    """Builds dataset for finetuning.
+
+    Args:
+      filenames: a list of strings, names of TFRecord files.
+
+    Returns:
+      dataset: a tf.data.Dataset instance, each item is a tuple of int tensors
+        of shape [batch_size, seq_len] holding the token ids, and of shape
+        [batch_size] holding sequence class labels.
+    """
+    filter_fn = None
+    dataset = create_and_preprocess(
+        filenames,
+        parse_fn=parse_fn_sequence_classification,
+        shuffle=self._shuffle,
+        num_parallel_calls=self._num_parallel_calls,
+        filter_fn=filter_fn,
+        buffer_size_per_file=250,
+        random_seed=self._random_seed)
+
+    def example_to_bucket_id(token_ids, _):
+      seq_len = tf.size(token_ids)
+      bucket_id = seq_len // self._bucket_width
+      return tf.cast(tf.minimum(self._num_buckets - 1, bucket_id), 'int64')
+
+    def batching_fn(bucket_id, grouped_dataset):
+      return grouped_dataset.padded_batch(
+          self._batch_size, ([None], [1]), drop_remainder=True)
+
+    dataset = dataset.apply(tf.data.experimental.group_by_window(
+        key_func=example_to_bucket_id,
+        reduce_func=batching_fn,
+        window_size=self._batch_size))
+    dataset = dataset.repeat(-1)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    return dataset
