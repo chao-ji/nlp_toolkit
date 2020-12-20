92d32dc708be8f04d31c06c0084ee4cc60d2cdf5
chaoji <chocobo1985@gmail.com>
2020-12-20 18:44:56 +0800

    added sampling functions

--- utils.py
+++ utils.py
@@ -108,3 +108,67 @@ def dict_to_example(dictionary):
   for k, v in dictionary.items():
     features[k] = tf.train.Feature(int64_list=tf.train.Int64List(value=v))
   return tf.train.Example(features=tf.train.Features(feature=features))
+
+
+def nucleus_sampling(scores, threshold=0.95):
+  """Sample from the head of the probability distribution that contains the 
+  vast majority of probability mass. See https://arxiv.org/abs/1904.09751 
+  for details. The distribution is truncated to the  and re-normalized.
+
+  Args:
+    scores: numpy array of shape [vocab_size], the probability distribution (
+      sum to one) of all possible next-tokens over the vocabulary.
+    threshold: float scalar, the minimum value of the sum of probability mass
+      that the head of the distribution must exceed. 
+
+  Returns:
+    next_token_id: int scalar, the sampled id of the next token.
+  """
+  ids = np.argsort(-scores)
+  cumsum = [0.] + np.cumsum(scores[ids]).tolist()
+  # search space is any value >= low and <= high
+  low, high = 0, len(cumsum) - 2
+
+  while low <= high:
+    mid = (low + high) // 2
+    sum1 = cumsum[mid]
+    sum2 = cumsum[mid + 1]
+    if sum1 < threshold and sum2 >= threshold:
+      break
+    elif sum2 < threshold: # exclude indices <= mid 
+      low = mid + 1
+    elif sum1 >= threshold: # exclude indices >= mid
+      high = mid - 1
+    else:
+      raise ValueError('Impossible outcome')
+
+  probs = scores[ids[:mid + 1]] / sum2
+  next_token_id = np.random.choice(ids[:mid + 1], p=probs)
+  return next_token_id
+
+
+def topk_sampling(scores, k=40):
+  """Sample from the top-k tokens with the largest probability. The distribution
+   is truncated and re-normalized.
+
+  Args:
+    scores: numpy array of shape [vocab_size], the probability distribution (
+      sum to one) of all possible next-tokens over the vocabulary.
+    k: int scalar, the num of next-tokens with largest probability to sample 
+      from.
+
+  Returns:
+    next_token_id: int scalar, the sampled id of the next token.
+  """
+  min_pq = list(zip(scores[:k], range(k)))
+  heapq.heapify(min_pq)
+  for i in np.arange(k, len(scores)):
+    if scores[i] > min_pq[0][0]:
+      min_pq[0] = scores[i], i
+      heapq.heapify(min_pq)
+
+  probs, ids = list(zip(*min_pq))
+  probs = np.array(probs)
+  probs /= probs.sum()
+  next_token_id = np.random.choice(ids, p=probs)
+  return next_token_id
