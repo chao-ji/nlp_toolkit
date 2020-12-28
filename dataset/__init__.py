dbe429b9f1feac6c35149ccca2de507954ea91e0
chaoji <chocobo1985@gmail.com>
2020-12-28 22:33:09 +0800

    update

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,3 +1,6 @@
 from .dataset_builder import DynamicBatchDatasetBuilder
 from .dataset_builder import StaticBatchDatasetBuilder
-from .dataset_builder import SequenceClassifierDatasetBuilder 
+from .dataset_builder import SequenceClassifierDatasetBuilder
+from .parse_fn import parse_fn_sequence_pair
+from .parse_fn import parse_fn_single_sequence
+from .parse_fn import parse_fn_sequence_classification  
